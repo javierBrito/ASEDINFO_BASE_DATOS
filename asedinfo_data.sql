c195682f1be61cd4d5884278769173d315a66a06
Javier Brito <vjbritoa@hotmail.com>
2023-12-13 09:34:59 -0500

    Ajustes en la url del whatsapp api

--- asedinfo_data.sql
+++ asedinfo_data.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 12-12-2023 a las 22:19:20
+-- Tiempo de generación: 13-12-2023 a las 15:33:17
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
